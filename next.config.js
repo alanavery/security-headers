c2f202b02b5fe2f6e5d4e1d6b8b502d70668d4f3
Alan Avery <delayedaa@gmail.com>
2022-10-05 15:19:23 -0500

    add nonce attribute to head

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 
+const contentSecurityPolicy = `
+  default-src 'self';
+  style-src 'self' 'nonce-test';
+`;
+
 const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: contentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
   {
     key: 'X-XSS-Protection',
     value: '1; mode=block',
