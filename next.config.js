cdb4f01003ed7804a49b0aff5c69a996bf082de0
Alan Avery <delayedaa@gmail.com>
2022-10-05 14:37:26 -0500

    add security header

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+];
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
